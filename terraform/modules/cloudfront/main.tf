434bd685ccb1e5071314c7df2472022d2df3f55a
Scott Lindsey <scott@scott.to>
2019-03-11 14:47:27 -0700

    why so hard

--- terraform/modules/cloudfront/main.tf
+++ terraform/modules/cloudfront/main.tf
@@ -78,7 +78,7 @@ resource "aws_cloudfront_distribution" "the-cloudfront-distribution" {
 
         forwarded_values {
             query_string = true
-            headers      = ["Access-Control-Request-Headers", "Access-Control-Request-Method", "Origin", "Host"]
+            headers      = ["Access-Control-Request-Headers", "Access-Control-Request-Method", "Host", "Origin"]
 
             cookies {
                 forward = "whitelist"
